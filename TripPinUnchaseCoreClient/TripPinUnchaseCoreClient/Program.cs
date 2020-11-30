fc992d275ff2d5863b18cbd0599be8604449d9a8
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-11-30 23:58:30 +0300

    Complete task2.

---
+++ TripPinUnchaseCoreClient/TripPinUnchaseCoreClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace TripPinUnchaseCoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string serviceUri = "https://services.odata.org/TripPinRESTierService";
+            var container = new Microsoft.OData.Service.Sample.TrippinInMemory.Models.Container(new Uri(serviceUri));
+
+            IAsyncResult asyncResult = container.People.BeginExecute((ar) =>
+            {
+                Console.WriteLine("People in TripPin service:");
+                var people = container.People.EndExecute(ar);
+
+                foreach (var person in people)
+                {
+                    Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
+                }
+
+            }, null);
+
+            WaitHandle.WaitAny(new[] { asyncResult.AsyncWaitHandle });
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
