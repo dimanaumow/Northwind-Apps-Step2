5f533f420c459430c6c165b28022dcc0fa8e4a1e
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-12-10 01:28:47 +0300

    Complete task3.

---
+++ NorthwindServiceFrameworkClient/NorthwindServiceFrameworkClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindServiceFrameworkClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string serviceUri = "https://services.odata.org/V3/Northwind/Northwind.svc/";
+            var entities = new NorthwindModel.NorthwindEntities(new Uri(serviceUri));
+
+            var employees = entities.Employees;
+            Console.WriteLine("Employees in Northwind service:");
+
+            foreach (var person in employees)
+            {
+                Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
