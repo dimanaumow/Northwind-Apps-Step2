43e931a2d131f7039ccaff6b09724fb7ea0c7847
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-12-10 12:57:31 +0300

    Complete task4.

---
+++ NorthwindServiceCoreClient/NorthwindServiceCoreClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace NorthwindServiceCoreClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string serviceUri = "https://services.odata.org/V3/Northwind/Northwind.svc/";
+            var entities = new NorthwindModel.NorthwindEntities(new Uri(serviceUri));
+
+            IAsyncResult asyncResult = entities.Employees.BeginExecute((ar) =>
+            {
+                Console.WriteLine("People in TripPin service:");
+                var people = entities.Employees.EndExecute(ar);
+
+                foreach (var person in people)
+                {
+                    Console.WriteLine("\t{0} {1}", person.FirstName, person.LastName);
+                }
+
+            }, null);
+
+            WaitHandle.WaitAny(new[] { asyncResult.AsyncWaitHandle });
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+        }
+    }
+}
